6a6e19e991e8b653d290398729c0088ea8dbf402
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-06 16:56:25 +0200

    feat: ajoute l'entité Quiz (titre, questions)

--- src/main/java/com/vacheronalyssa/quizappback/entity/Quiz.java
+++ src/main/java/com/vacheronalyssa/quizappback/entity/Quiz.java
@@ -1,9 +1,7 @@
 package com.vacheronalyssa.quizappback.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
