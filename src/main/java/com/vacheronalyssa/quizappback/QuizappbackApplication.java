684049cca1e56942f57f8473a9c01370405605f3
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-06 16:50:58 +0200

    first commit

---
+++ src/main/java/com/vacheronalyssa/quizappback/QuizappbackApplication.java
@@ -0,0 +1,13 @@
+package com.vacheronalyssa.quizappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacheronalyssa/quizappback/QuizappbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacheronalyssa.quizappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
